28b5306ed79ca336920e5f678d8774f3fe5ff073
Oleh Korshun <oleg.korshun@outlook.com>
2019-11-21 21:44:50 +0300

    Updates

---
+++ precache-manifest.e5609cf5be17e6b356a4d100a175d38e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16794828f93814d26667fa8894e955e3",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "3004439f990689ad5f35",
+    "url": "/star-db/static/css/main.23d4db79.chunk.css"
+  },
+  {
+    "revision": "67ddd0e45f058b9fb013",
+    "url": "/star-db/static/js/2.94b9d967.chunk.js"
+  },
+  {
+    "revision": "3004439f990689ad5f35",
+    "url": "/star-db/static/js/main.9e697ea2.chunk.js"
+  },
+  {
+    "revision": "0116862b95b85cdbd8c4",
+    "url": "/star-db/static/js/runtime-main.b6f34180.js"
+  }
+]);
